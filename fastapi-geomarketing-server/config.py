559e107dde1e375718244371946c24c88e87aaf6
Maksim Makarov <maximmakarov1996@gmail.com>
2021-08-04 13:03:36 +0300

    Update config.py

--- fastapi-geomarketing-server/config.py
+++ fastapi-geomarketing-server/config.py
@@ -1,3 +1,3 @@
-API_KEY = "idhlDg7Y08LoZwNQ6UCPfq5bsxtrbLMpJZi58GyIcIQ"
+API_KEY = "YOUR_API_KEY"
 PLACES_ENDPOINT = "https://browse.search.hereapi.com/v1/browse"
 ISOLINE_ENDPOINT = "https://isoline.router.hereapi.com/v8/isolines"
